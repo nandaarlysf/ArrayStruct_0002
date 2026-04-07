261bc8c0e17e6f15b6531f4b39afe5d6d08f9d66
nandaarlysf <nandaarlys36@gmail.com>
2026-04-07 14:47:25 +0700

    mengetahui deklarasi array

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Deklarasi Array dengan inisialisasi
+    int dataku[5]={10,20,30,40,50};
+    //Deklarasi tanpa inisialisasi
+    int datamu[5];
+    int x ;
+    //cara membaca array dataku
+}
