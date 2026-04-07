ce38b228bf1f12a2425a47fedb1b70eaaa1c9b56
nandaarlysf <nandaarlys36@gmail.com>
2026-04-07 15:21:04 +0700

    declarasi obyek struct

--- structstruct.cpp
+++ structstruct.cpp
@@ -10,3 +10,23 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+int main()
+{
+    //Deklarasi obyek dari struct orang 
+    orang mhs;
+    //isi objek
+    cout<<"Nama Mahasiswa:";
+    cin>>mhs.nama;
+    cout<<"Alamat Desa:";
+    cin>>mhs.alamat.desa;
+    cout<<mhs."alamat kota:";
+    cin>>mhs.alamat.kota;
+    cout>>"Umur:";
+    cin<<mhs.umur;
+    cout>>endl;
+    //Menampilkan Isi obyek 
+    cout<<"Nama:"<<mhs.nama<<endl;
+    cout<<"Desa:"<<mhs.alamat.desa<<endl;
+    cout<<"kota:"<<mhs.alamat.kota<<endl;
+    cout<<"umur:"<<mhs.umur<<endl; 
+}
