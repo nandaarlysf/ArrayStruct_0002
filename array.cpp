ab17b1d4bc030c7310de5e740dfb3c4a08562cd0
nandaarlysf <nandaarlys36@gmail.com>
2026-04-07 14:54:24 +0700

    untuk membaca  array

--- array.cpp
+++ array.cpp
@@ -8,4 +8,12 @@ int main(){
     int datamu[5];
     int x ;
     //cara membaca array dataku
+    cout<<"Data ke-3:"<<dataku[2]<<endl;
+
+    //operasi Matematika
+    x=dataku[2]*3;
+    //cara menulis atau mengisi array
+    dataku[2]=600;
+    cout<<"Data ke -3 yang baru:"<<dataku[2]<<endl;
+    
 }
